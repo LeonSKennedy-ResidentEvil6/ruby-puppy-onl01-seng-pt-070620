a89529606a692321616298d1fb7ccff87b0643c9
Ebo Lee <yiboli.coding@gmail.com>
2020-07-27 20:34:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,14 +14,15 @@ class Dog
     @@all
   end 
   
-  def self.clear_all
-    @@all =[]
-  end 
    
   def self.print_all
     @@all.each do |dog| 
       puts dog.name
     end 
+  end 
+  
+  def self.clear_all
+    @@all.clear
   end 
 
  def save
